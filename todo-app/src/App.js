1bb8d513d7d3790c62975e5d1846855ef0f511e2
Carter Clifford <carterlc@live.ca>
2023-03-07 10:04:47 -0600

    updates the form to allow the user to enter a new ToDo item.

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form>
+        <input type="text" value={newTodo} onChange={(event) => setNewTodo(event.target.value)} />
+        <button type="submit">Add To Do</button>
+      </form>
     </div>
   );
 }
