61ccd39a389143a94ff05f2d72d2cac33c86df53
Carter Clifford <carterlc@live.ca>
2023-03-07 10:19:47 -0600

    Has delete bug where the last todo get deleted when you press enter.

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -4,11 +4,48 @@ function App() {
 
   const [todos, setTodos] = useState([]);
   const [newTodo, setNewTodo] = useState("");
-  
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setTodos([...todos, { id: Date.now(), text: newTodo, completed: false }]);
+    setNewTodo("");
+  };
+
+  const handleToggleCompleted = (id) => {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed: !todo.completed };
+        } else {
+          return todo;
+        }
+      })
+    );
+  };
+
+  const handleDeleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input type="text" value={newTodo} onChange={(event) => setNewTodo(event.target.value)} />
+        <ul>
+          {todos.map((todo) => (
+            <li key={todo.id}>
+              <input
+                type="checkbox"
+                checked={todo.completed}
+                onChange={() => handleToggleCompleted(todo.id)}
+              />
+              <span style={{ textDecoration: todo.completed ? "line-through" : "none" }}>
+                {todo.text}
+              </span>
+              <button onClick={() => handleDeleteTodo(todo.id)}>Delete</button>
+            </li>
+          ))}
+        </ul>
         <button type="submit">Add To Do</button>
       </form>
     </div>
